e0934a55175fe512e6cd078abe12829633a732fd
Len Boyette <boyettel@gmail.com>
2022-01-08 12:58:30 -0500

    format, custom, combine_columns

--- xforms/__init__.py
+++ xforms/__init__.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
@@ -70,8 +70,8 @@ def datediff_new(ds, new_col, start, end, increment):
 
     rc = ds
     rc[new_col] = rc[end] - rc[start]
-    rc[new_col] = rc[new_col] / numpy.timedelta64(1, inc)
-    rc[new_col] = rc[new_col].apply(numpy.floor)
+    rc[new_col] = rc[new_col] / np.timedelta64(1, inc)
+    rc[new_col] = rc[new_col].apply(np.floor)
     return rc
 
 def substr_new(ds, new_col, source, start=None, end=None):
@@ -104,14 +104,42 @@ def round(ds, col, places):
 def substr(ds, col, start=None, end=None):
     return substr_new(ds, col, col, start, end)
 
-def format():
-    pass
+def format(ds, col, arg):
+    if arg != 0:
+        raise Exception(f'edit_column_format does not work for non-zero args {arg}')
+    rc = ds
+    rc[col] = ds[col].apply(np.floor)
+    return rc
 
-def custom():
-    pass
+def custom(ds, col, function):
+    return custom_new(ds, col, function)
 
-def combine_columns():
-    pass
+def combine_columns(ds, new_col, columns, separator=',', operator='concatenate', hide_columns=False):
+    rc = ds
+
+    if operator == 'concatenate':
+        for col in columns:
+            rc[col] = rc[col].astype(str)
+        rc[new_col] = rc[columns].agg(separator.join, axis=1)
+    
+    elif operator in ('add', 'subtract', 'multiply'):
+        cols = [ds[col] for col in columns]
+        rc[new_col] = cols[0]
+        for col in cols[1:]:
+            if operator == 'add':
+                rc[new_col] = rc[new_col].add(col)
+            elif operator == 'subtract':
+                rc[new_col] = rc[new_col].sub(col)
+            elif operator == 'multiply':
+                rc[new_col] = rc[new_col].multiply(col)
+
+    else:
+        raise Exception(operator + ' is unsupported')
+
+    if hide_columns:
+        rc = ds.drop(columns, errors='ignore')
+
+    return rc
 
 def unpivot():
     pass
