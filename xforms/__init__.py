badf1add85bd8b8d200ef9e56a85b07137004e55
jgoel <jay@hotswap.app>
2022-01-07 21:29:38 -0500

    remove columns, line

--- xforms/__init__.py
+++ xforms/__init__.py
@@ -5,3 +5,28 @@ def divide(ds, new_col, dividend, divisor):
     rc = ds
     rc[new_col] = ds[dividend] / ds[divisor]
     return rc
+
+def remove_columns(ds, columns):
+    cols_to_drop = [c for c in columns if c in ds]
+    rc = ds.drop(columns=cols_to_drop, errors='ignore')
+    return rc
+
+def line(ds):
+    # Ensure the default "index" column is not part of the plot\n'
+    if type(ds.index) == pd.RangeIndex or type(ds.index) == pd.Int64Index:
+        index_column = ds.columns[0]
+        ds = ds.set_index(index_column)
+
+    # Configure chart display
+    fig, ax = plt.subplots()
+    ax.yaxis.set_major_formatter(number_formatter)
+    ds.plot.line(ax=ax)
+
+    # Rotate x axis labels 45 degrees
+    fig.autofmt_xdate()
+
+    # Increase chart size
+    fig.set_size_inches(12, 8)
+
+    # Display chart
+    plt.show()
