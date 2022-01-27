f2fe581aeacd92c1fa33c3c2d1b8aba7b5b9c628
jgoel <jay@hotswap.app>
2022-01-27 13:14:06 -0500

    import ipython libs

--- xforms/__init__.py
+++ xforms/__init__.py
@@ -8,6 +8,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import sqlite3
+from IPython.core.display import display, HTML
 
 
 def adapter(fn):
